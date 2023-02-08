ec77a007a52462abe173bbeb02c113071ed0e664
rober <robertf.65@hotmail.fr>
2023-02-08 19:11:38 +0100

    refactor import + tag class test pour algo sort

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,11 +2,8 @@ import java.util.Arrays;
 
 public class MainClass {
         public static void main(String[] args) {
-                int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 };
-                int[] arr2 = arr.clone();
-                System.out.println(Arrays.toString(arr2));
-                arr[0] *= 100;
+                int[] arr = { 103, 13, 7, 6, 45, 21, 9, 101, 102 };
+                Arrays.sort(arr);
                 System.out.println(Arrays.toString(arr));
-                System.out.println(Arrays.toString(arr2));
         }
 }
--- src/test/java/sort/SortAlgorithmsTest.java
+++ src/test/java/sort/SortAlgorithmsTest.java
@@ -1,21 +1,28 @@
 package sort;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.testng.annotations.BeforeTest;
-
+import org.junit.jupiter.api.TestInstance;
 import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SortAlgorithmsTest {
     SortAlgorithms sa = new SortAlgorithms();
-    int[] arrayTest = new int[]{2, 6, 9, 8, 11, 23, 65, 89, 14, 47, 85};
+    int[] arrayTest = {2, 6, 9, 8, 11, 23, 65, 89, 14, 47, 85};
     int[] arrayTestToCompare = arrayTest.clone();
 
-    @BeforeTest
-    public void init(){
+    @BeforeAll
+    public void setup(){
+        Arrays.sort(arrayTestToCompare);
+    }
+    @BeforeEach
+    public void resetState(){
         arrayTest = new int[]{2, 6, 9, 8, 11, 23, 65, 89, 14, 47, 85};
 
     }
+
     @Test
     void insertionSort() {
         sa.insertionSort(arrayTest);
